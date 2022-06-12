78199b1e3eb3572d5bf1e9701df2fc5034f711fa
septianmalela <maulanas430@gmail.com>
2022-06-12 19:26:52 +0530

    Set devise login, signup users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+         sessions: 'users/devise/sessions',
+    registrations: 'users/devise/registrations',
+      invitations: 'users/devise/invitations',
+        passwords: 'users/devise/passwords'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :homes
+
+  root 'homes#index'
 end
